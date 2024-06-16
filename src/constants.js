43cca97892a13b2a1bfa64050c35d7e8b0175ff6
S Faizaan Hussain <hussainfaizaan07@gmail.com>
2024-06-16 18:01:42 +0530

    created auth middleware and password encryption and verification methods

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
 export const DB_NAME = "Edypros"
+
+export const DICEBEAR_API_URI = "https://api.dicebear.com/9.x/initials/svg?radius=50&fontFamily=Verdana&seed="
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,61 +1,99 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     lastName: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: true,
-        trim: true,
-        index: true
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: true,
+      trim: true,
+      index: true,
     },
     password: {
-        type:String,
-        required: true,
+      type: String,
+      required: true,
     },
-    accountType : {
-        type: String,
-        required: true,
-        trim: true,
-        enum: ["Admin", "Student", "Instructor"]
+    accountType: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: ["Admin", "Student", "Instructor"],
     },
     avatar: {
-        type: String,
+      type: String,
     },
     refreshTokens: {
-        type: String, 
+      type: String,
     },
     registeredCourses: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Course"
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
     ],
     profile: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Profile",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Profile",
+      required: true,
     },
     courseProgress: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "CourseProgress"
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "CourseProgress",
+      },
     ],
     isActive: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-}, {timestamps : true})
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
