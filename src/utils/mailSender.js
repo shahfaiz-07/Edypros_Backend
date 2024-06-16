40a27a5d4447f4d03b6efbfd06df570358cacaf1
S Faizaan Hussain <hussainfaizaan07@gmail.com>
2024-06-16 16:24:44 +0530

    created necessary utility functions and mailSender

---
+++ src/utils/mailSender.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer"
+import "colors"
+
+export const sendMail = async (to, subject, body) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        })
+        const info = await transporter.sendMail({
+            from:process.env.MAIL_USER,
+            to,
+            subject,
+            html: body
+        });
+        console.log(info)
+    } catch(err) {
+        console.log(`Some Error Occured while sending email !!`.bgRed);
+        console.error(`ERROR : ${err.message}`);
+    }
+}
